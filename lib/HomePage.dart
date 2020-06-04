d992cc3c61f56e3b02297abd40a129bc4002390a
unknown <nottiffchan@gmail.com>
2020-06-05 00:07:42 +0800

    drop down menu, tile coloring

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -11,46 +11,57 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-
-  var tiles = <Tile>[
-    new Tile('Meat', red, ("assets/meat.png"), name: 'Beef'),
-    new Tile('Fruits', red, ("assets/banana.png"), name: 'Banana'),
-    new Tile('Dry Goods', grey, ("assets/flour.png"), name: 'Flour'),
-    new Tile('Vegetable', grey, ("assets/broccoli.png"), name: 'Broccoli'),
-    new Tile('Dairy', grey, ("assets/cheese.png"), name: 'Cheese'),
-    new Tile('Dairy', grey, ("assets/milk.png"), name: 'Milk'),
-    new Tile('Dry Goods', red, ("assets/noodles.png"), name: 'Noodles'),
-
-
-    new Tile('Dairy', red, ("assets/lobster.png"), name: 'Lobster'),
-    new Tile('Dairy', grey, ("assets/beer.png"), name: 'Beer'),
-    new Tile('Dairy', grey, ("assets/bread.png"), name: 'Bread'),
-    new Tile('Dairy', grey, ("assets/fish.png"), name: 'Fish'),
-    new Tile('Dairy', grey, ("assets/butter.png"), name: 'Butter'),
-    new Tile('Dairy', grey, ("assets/eggs.png"), name: 'Eggs'),
-    new Tile('Dairy', grey, ("assets/ice-cream.png"), name: 'Ice Cream'),
-    new Tile('Dairy', grey, ("assets/mushroom.png"), name: 'Mushroom'),
-    new Tile('Dairy', grey, ("assets/cherries.png"), name: 'Cherry'),
-    new Tile('Dairy', grey, ("assets/chicken.png"), name: 'Chicken'),
-    new Tile('Dairy', grey, ("assets/chips.png"), name: 'Chips'),
-    new Tile('Dairy', grey, ("assets/chocolate.png"), name: 'Chocolate'),
-    new Tile('Dairy', grey, ("assets/coffee.png"), name: 'Coffee'),
-    new Tile('Dairy', grey, ("assets/coke.png"), name: 'Coke'),
-    new Tile('Dairy', grey, ("assets/fruits.png"), name: 'Apple'),
-    new Tile('Dairy', grey, ("assets/juice.png"), name: 'Apple Juice'),
-    new Tile('Dairy', grey, ("assets/onion.png"), name: 'Onion'),
-    new Tile('Dairy', grey, ("assets/rice.png"), name: 'Rice'),
-    new Tile('Dairy', grey, ("assets/tea.png"), name: 'Tea'),
-
-
+  var addedToList = <Tile>[
 
   ];
 
+  var fruitsVegetables = <Tile>[
+    new Tile(grey, ("assets/banana.png"), name: 'Banana'),
+    new Tile(grey, ("assets/broccoli.png"), name: 'Broccoli'),
+    new Tile(grey,("assets/cherries.png"), name: 'Cherry'),
+    new Tile(grey,("assets/mushroom.png"), name: 'Mushroom'),
+  ];
+
+  var meatFish = <Tile>[
+    new Tile(grey,("assets/meat.png"), name: 'Beef'),
+    new Tile(grey,("assets/chicken.png"), name: 'Chicken'),
+    new Tile(grey,("assets/eggs.png"), name: 'Eggs'),
+    new Tile(grey,("assets/fish.png"), name: 'Fish'),
+    new Tile(grey,("assets/lobster.png"), name: 'Lobster'),
+  ];
+
+  var dairy = <Tile>[
+    new Tile(grey,("assets/butter.png"), name: 'Butter'),
+    new Tile(grey,("assets/cheese.png"), name: 'Cheese'),
+    new Tile(grey,("assets/milk.png"), name: 'Milk'),
+  ];
+
+  var dryGoods = <Tile>[
+    new Tile(grey,("assets/bread.png"), name: 'Bread'),
+    new Tile(grey,("assets/flour.png"), name: 'Flour'),
+    new Tile(grey,("assets/noodles.png"), name: 'Noodles'),
+    new Tile(grey,("assets/rice.png"), name: 'Rice'),
+    new Tile(grey,("assets/onion.png"), name: 'Onion'),
+  ];
+
+  var beverages = <Tile>[
+    new Tile(grey,("assets/beer.png"), name: 'Beer'),
+    new Tile(grey,("assets/coffee.png"), name: 'Coffee'),
+    new Tile(grey,("assets/juice.png"), name: 'Juice'),
+    new Tile(grey,("assets/coke.png"), name: 'Soft Drink'),
+    new Tile(grey,("assets/tea.png"), name: 'Tea'),
+  ];
+
+  var snacksSweets = <Tile>[
+    new Tile(grey,("assets/ice-cream.png"), name: 'Ice Cream'),
+    new Tile(grey,("assets/chips.png"), name: 'Chips'),
+    new Tile(grey,("assets/chocolate.png"), name: 'Chocolate'),
+  ];
 
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: NestedScrollView(
@@ -67,10 +78,11 @@ class _HomePageState extends State<HomePage> {
                 centerTitle: true,
                 title: Container(
                   width: MediaQuery.of(context).size.width * 0.63,
+                  height: MediaQuery.of(context).size.height * 0.04,
                   child: CupertinoTextField(
                     style: GoogleFonts.mavenPro(),
                     keyboardType: TextInputType.text,
-                    placeholder: "Buy what shit",
+                    placeholder: "What u want buy",
                     placeholderStyle: TextStyle(
                       color: Colors.white,
                       fontSize: 14.0,
@@ -93,34 +105,209 @@ class _HomePageState extends State<HomePage> {
         },
         body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                child: GridView(
-                  scrollDirection: Axis.vertical,
-                  physics: ClampingScrollPhysics(),
-                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent:125,
-                    mainAxisSpacing: 6,
-                    crossAxisSpacing: 6,
-                  ),
-                  children: List.generate(tiles.length, (index) {
-                    return GridTile(
-                      child: Tile(
-                        tiles[index].category,
-                        tiles[index].bg,
-                        tiles[index].img, 
-                        name: tiles[index].name,
-                      )
-                    );
-                  }
-                  ),
-                ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              GridView.count(
+                shrinkWrap: true,
+                physics: ClampingScrollPhysics(),
+                crossAxisCount: 3,
+                padding: const EdgeInsets.all(6),
+                crossAxisSpacing: 6,
+                mainAxisSpacing: 6,
+                childAspectRatio: 1,
+                children: List.generate(addedToList.length, (index) {
+                  return GridTile(
+                    child: Tile(
+                      grey,
+                      addedToList[index].img, 
+                      name: addedToList[index].name,
+                    )
+                  );
+                },
+                )
               ),
-            )
-          ],
-        ),
+              ExpansionTile(
+                title: Text("Recently Used", style: GoogleFonts.mavenPro(fontSize: 20)),
+                // leading: Icon(Icons.child_care),
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Text("BANANA", style: GoogleFonts.mavenPro(fontSize: 20, fontWeight: FontWeight.bold),),
+                      Container(width: 100, child: Image.asset("assets/banana.png"))
+                    ],
+                  )
+                ],
+                backgroundColor: Colors.white,
+                initiallyExpanded: false,
+              ),
+              ExpansionTile(
+                title: Text("Fruits and Vegetables", style: GoogleFonts.mavenPro(fontSize: 20)),
+                // leading: Icon(Icons.child_care),
+                children: <Widget>[
+                  GridView.count(
+                    shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    crossAxisCount: 3,
+                    padding: const EdgeInsets.all(6),
+                    crossAxisSpacing: 6,
+                    mainAxisSpacing: 6,
+                    childAspectRatio: 1,
+                    children: List.generate(fruitsVegetables.length, (index) {
+                      return GridTile(
+                        child: Tile(
+                          grey,
+                          fruitsVegetables[index].img, 
+                          name: fruitsVegetables[index].name,
+                        )
+                      );
+                    },
+                    )
+                  ),
+                ],
+                backgroundColor: Colors.white,
+                initiallyExpanded: false,
+              ),
+              ExpansionTile(
+                title: Text("Meat and Fish", style: GoogleFonts.mavenPro(fontSize: 20)),
+                children: <Widget>[
+                  GridView.count(
+                    shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    crossAxisCount: 3,
+                    padding: const EdgeInsets.all(6),
+                    crossAxisSpacing: 6,
+                    mainAxisSpacing: 6,
+                    childAspectRatio: 1,
+                    children: List.generate(meatFish.length, (index) {
+                      return GridTile(
+                        child: Tile(
+                          grey,
+                          meatFish[index].img, 
+                          name: meatFish[index].name,
+                        )
+                      );
+                    },
+                    )
+                  ),
+                ],
+                backgroundColor: Colors.white,
+                initiallyExpanded: false,
+              ),
+              ExpansionTile(
+                title: Text("Dairy", style: GoogleFonts.mavenPro(fontSize: 20)),
+                // leading: Icon(Icons.child_care),
+                children: <Widget>[
+                  GridView.count(
+                    shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    crossAxisCount: 3,
+                    padding: const EdgeInsets.all(6),
+                    crossAxisSpacing: 6,
+                    mainAxisSpacing: 6,
+                    childAspectRatio: 1,
+                    children: List.generate(dairy.length, (index) {
+                      return GridTile(
+                        child: Tile(
+                          grey,
+                          dairy[index].img, 
+                          name: dairy[index].name,
+                        )
+                      );
+                    },
+                    )
+                  ),
+                ],
+                backgroundColor: Colors.white,
+                initiallyExpanded: false,
+              ),
+
+              ExpansionTile(
+                title: Text("Dry Goods", style: GoogleFonts.mavenPro(fontSize: 20)),
+                // leading: Icon(Icons.child_care),
+                children: <Widget>[
+                  GridView.count(
+                    shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    crossAxisCount: 3,
+                    padding: const EdgeInsets.all(6),
+                    crossAxisSpacing: 6,
+                    mainAxisSpacing: 6,
+                    childAspectRatio: 1,
+                    children: List.generate(dryGoods.length, (index) {
+                      return GridTile(
+                        child: Tile(
+                          grey,
+                          dryGoods[index].img, 
+                          name: dryGoods[index].name,
+                        )
+                      );
+                    },
+                    )
+                  ),
+                ],
+                backgroundColor: Colors.white,
+                initiallyExpanded: false,
+              ),
+
+              ExpansionTile(
+                title: Text("Snacks and Sweets", style: GoogleFonts.mavenPro(fontSize: 20)),
+                // leading: Icon(Icons.child_care),
+                children: <Widget>[
+                  GridView.count(
+                    shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    crossAxisCount: 3,
+                    padding: const EdgeInsets.all(6),
+                    crossAxisSpacing: 6,
+                    mainAxisSpacing: 6,
+                    childAspectRatio: 1,
+                    children: List.generate(snacksSweets.length, (index) {
+                      return GridTile(
+                        child: Tile(
+                          grey,
+                          snacksSweets[index].img, 
+                          name: snacksSweets[index].name,
+                        )
+                      );
+                    },
+                    )
+                  ),
+                ],
+                backgroundColor: Colors.white,
+                initiallyExpanded: false,
+              ),
+
+              ExpansionTile(
+                title: Text("Beverages", style: GoogleFonts.mavenPro(fontSize: 20)),
+                // leading: Icon(Icons.child_care),
+                children: <Widget>[
+                  GridView.count(
+                    shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    crossAxisCount: 3,
+                    padding: const EdgeInsets.all(6),
+                    crossAxisSpacing: 6,
+                    mainAxisSpacing: 6,
+                    childAspectRatio: 1,
+                    children: List.generate(beverages.length, (index) {
+                      return GridTile(
+                        child: Tile(
+                          grey,
+                          beverages[index].img, 
+                          name: beverages[index].name,
+                        )
+                      );
+                    },
+                    )
+                  ),
+                ],
+                backgroundColor: Colors.white,
+                initiallyExpanded: false,
+              ),
+            ],
+          ),
+        )
       ),
 
       ),
--- lib/Widgets/Tiles.dart
+++ lib/Widgets/Tiles.dart
@@ -1,51 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:leeks/constants.dart';
 
 class Tile extends StatefulWidget {
-  final String category, name;
-  final Color bg;
+  final String name;
   final String img;
+  Color bg = grey;
 
-  const Tile(this.category, this.bg, this.img, {this.name});
+  Tile(this.bg, this.img, {this.name});
 
   @override
   _TileState createState() => _TileState();
 }
 
 class _TileState extends State<Tile> {
+  Color c = grey;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: widget.bg,
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(bottom: 15),
-            child: Image.asset(widget.img),
-            width: MediaQuery.of(context).size.width * 0.17,
-          ),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                // Text(widget.category),
-                Text(
-                  widget.name, 
-                  style: GoogleFonts.mavenPro(
-                    textStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
-                    fontSize: 19,
-                    
-                  ),
-                ),
-              ],
+    return InkWell(
+      child: Container(
+        color: widget.bg,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(bottom: 15),
+              child: Image.asset(widget.img),
+              width: MediaQuery.of(context).size.width * 0.17,
             ),
-          ),
-        ]
-      )
+
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Text(
+                    widget.name, 
+                    style: GoogleFonts.mavenPro(
+                      textStyle: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+                      fontSize: 19,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            
+          ]
+        )
+      ),
+      onTap: () {
+        setState(() {
+          widget.bg == grey ? widget.bg = red : widget.bg = grey;
+        });
+      },
     );
+
   }
 }
 
